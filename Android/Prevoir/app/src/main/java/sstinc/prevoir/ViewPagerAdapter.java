865fb88cd34ffdf48e72fc1295ba6e1fe3bb7a6b
MoonCheesez <joeltiojinhon@gmail.com>
2016-10-13 21:04:07 +0800

    Fixed plan, tabs said they include settings which they do not. Added Android project directory as well.

---
+++ Android/Prevoir/app/src/main/java/sstinc/prevoir/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package sstinc.prevoir;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabTitles = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title) {
+        this.fragments.add(fragment);
+        this.tabTitles.add(title);
+    }
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles.get(position);
+    }
+}
