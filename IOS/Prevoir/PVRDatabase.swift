2d3d6f6934dfe225d35af929fabb4db543dcb16a
Zhu Zhan Yan <program.nom@gmail.com>
2016-10-14 21:53:25 +0800

    Refratored Database Init, Added Staging and I/O methods

--- IOS/Prevoir/PVRDatabase.swift
+++ IOS/Prevoir/PVRDatabase.swift
@@ -8,63 +8,152 @@
 
 import UIKit
 
+public enum PVRDBKey:String
+{
+    case task = "pvrdb_task"
+    case cache = "pvrdb_cache"
+}
+
 public class PVRDatabase:NSObject
 {
     //Data
-    var task:[String:PVRTask] //Tasks
-    var mcache:[String:Any]  //In-Memory Cache
-    var cache:[String:NSCoding] //Cache
+    var task:[String:PVRTask] = [:] //Tasks
+    var mcache:[String:Any] = [:]  //In-Memory Cache
+    var cache:[String:NSCoding] = [:]//Cache
     
     //Storage
+    var stage:Bool = false
+    
     var pst_file_path:String
+    var pst_file_data:NSMutableData!
+    var pst_ach:NSKeyedArchiver!
+    var pst_modified:Bool = false
+
     var tmp_file_path:String
+    var tmp_file_data:NSMutableData!
+    var tmp_ach:NSKeyedArchiver!
+    var tmp_modified:Bool = false
+    
+    //Init
     
     override init()
     {
         //Persistent Storage
         let doc_path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
         self.pst_file_path = "\(doc_path)/pvr_db.plist"
-        
-        if FileManager.default.fileExists(atPath: self.pst_file_path)
-        {
-            if let ua = NSKeyedUnarchiver.unarchiveObject(withFile: self.pst_file_path) as? NSKeyedUnarchiver
-            {
-                self.task = (ua.decodeObject(forKey: "task") as! [String:PVRTask])
-            }
-            else
-            {
-                print("ERROR: Failed to load Persistent Data from: \(self.pst_file_path)")
-                abort()
-            }
-        }
-        else
-        {
-            self.task = [:]
-        }
-    
+
         //Cache (Tmp Storage)
         let tmp_path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
         self.tmp_file_path = "\(tmp_path)/pvr_cache.plist"
+
+        super.init()
         
-        if FileManager.default.fileExists(atPath: self.tmp_file_path)
+        self.load()
+    }
+    
+    //Staging
+    
+    public func stage_data(key:PVRDBKey,val:Any,file_path:String)
+    {
+        if self.stage == false
         {
-            if let ua = NSKeyedUnarchiver.unarchiveObject(withFile: self.tmp_file_path) as? NSKeyedArchiver
+            //New Stage
+            if file_path == self.pst_file_path
             {
-                self.cache = (ua.decodeObject(forKey: "task") as! [String:PVRTask])
+                self.pst_file_data = NSMutableData()
+                self.pst_ach = NSKeyedArchiver(forWritingWith: self.pst_file_data)
             }
-            else
+            else if file_path == self.tmp_file_path
             {
-                print("Info: Failed to load Tmp Data from: \(self.tmp_file_path)")
-                
-                self.cache = [:]
+                self.tmp_file_data = NSMutableData()
+                self.tmp_ach = NSKeyedArchiver(forWritingWith: self.tmp_file_data)
             }
+            
+            self.stage = true
+        }
+        
+        if file_path == self.pst_file_path, self.pst_modified == true
+        {
+            self.pst_ach.encode(val, forKey: key.rawValue)
+            
+        }
+        else if file_path == self.tmp_file_path, self.pst_modified == true
+        {
+            self.tmp_ach.encode(val, forKey: key.rawValue)
+        }
+        
+    }
+    
+    //I/O
+
+    public func load_data(key:PVRDBKey,file_path:String) -> Any?
+    {
+        if FileManager.default.fileExists(atPath: file_path)
+        {
+            if let ua = NSKeyedUnarchiver.unarchiveObject(withFile: file_path) as? NSKeyedUnarchiver
+            {
+                return ua.decodeObject(forKey: key.rawValue)
+            }
+        }
+        
+        return nil
+    }
+
+    
+    public func commit_data(file_path:String)
+    {
+        //Remove Old File
+        if FileManager.default.fileExists(atPath: file_path)
+        {
+            try! FileManager.default.removeItem(atPath: file_path)
+        }
+        
+        if file_path == self.pst_file_path
+        {
+            self.pst_ach.finishEncoding()
+            self.pst_file_data.write(toFile: file_path, atomically: true)
+            
+            self.stage = false
         }
         else
         {
-            self.cache = [:]
+            self.tmp_ach.finishEncoding()
+            self.tmp_file_data.write(toFile: file_path, atomically: true)
+            
+            self.stage = false
+        }
+    }
+
+    public func commit()
+    {
+        self.stage_data(key: PVRDBKey.task, val: self.task, file_path: self.pst_file_path)
+        self.stage_data(key: PVRDBKey.cache, val: self.cache, file_path: self.tmp_file_path)
+        
+        self.commit_data(file_path: self.pst_file_path)
+        self.commit_data(file_path: self.tmp_file_path)
+    }
+    
+    public func load()
+    {
+        //Persistent Storage
+        if (UIApplication.shared.delegate as! AppDelegate).use_cnt > 1
+        {
+            if let tsk = self.load_data(key: PVRDBKey.task, file_path: self.pst_file_path)
+            {
+                self.task = (tsk as! [String : PVRTask])
+            }
+            else
+            {
+                abort() //Task List Failed to Load
+            }
         }
         
-        //In-Memory Cache
-        self.mcache = [:]
+        //Cache (Tmp Storage)
+        if (UIApplication.shared.delegate as! AppDelegate).use_cnt > 1, let cch = self.load_data(key: PVRDBKey.cache, file_path: self.tmp_file_path)
+        {
+            self.cache = (cch as! [String:NSCoding])
+        }
     }
+    
+    //Data Manipulation
 }
