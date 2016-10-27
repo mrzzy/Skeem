bfbb0cea3619af5b3339ac78c68c71a0bbbc2933
MoonCheesez <joeltiojinhon@gmail.com>
2016-10-27 23:05:20 +0800

    Redid Datetime class. Classes dependent on Datetime are not updated.

--- Android/Prevoir/app/src/main/java/sstinc/prevoir/Datetime.java
+++ Android/Prevoir/app/src/main/java/sstinc/prevoir/Datetime.java
@@ -1,241 +1,296 @@
 package sstinc.prevoir;
 
 import java.util.Calendar;
+import java.util.Locale;
 
+//TODO: Locale datetime string format
+//TODO: Check dependency with Duration
+
+/**
+ * This class handles information relating to an instance in time using
+ * Calendar. Minutes is the smallest denomination this class handles.
+ *
+ * @see Calendar
+ */
 class Datetime {
-    private boolean hasDate;
-    private int year;
-    private int month;
-    private int day;
-    // Hour is in 24 hour
-    private boolean hasTime;
-    private int hour;
-    private int minute;
-
     private Calendar calendar;
+    private boolean hasDate;
+    private boolean hasTime;
 
+    /**
+     * Default constructor. Sets the calendar to the minimum date possible
+     * for calendar to hold. {@link #hasDate} and {@link #hasTime} are false
+     * to indicate absence of date and time.
+     */
     Datetime() {
         this.hasDate = false;
-        this.year = 0;
-        this.month = 0;
-        this.day = 0;
-
         this.hasTime = false;
-        this.hour = 0;
-        this.minute = 0;
 
         this.calendar = Calendar.getInstance();
-        this.calendar.set(this.year, this.month, this.day,
-                this.hour, this.minute);
+        this.calendar.set(0, 0, 0, 0, 0);
     }
 
+    // Copy constructor
     Datetime(Datetime datetime) {
-        this.hasDate = datetime.hasDate();
-        this.year = datetime.getYear();
-        this.month = datetime.getMonth();
-        this.day = datetime.getDay();
-
-        this.hasTime = datetime.hasTime();
-        this.hour = datetime.getHour();
-        this.minute = datetime.getMinute();
-
         this.calendar = Calendar.getInstance();
-        this.calendar.set(this.year, this.month, this.day,
-                this.hour, this.minute);
+        this.calendar.setTimeInMillis(datetime.getMillis());
+
+        this.hasDate = datetime.getHasDate();
+        this.hasTime = datetime.getHasTime();
     }
 
-    Datetime(int year, int month, int day) {
-        this.hasDate = true;
-        this.year = year;
-        this.month = month;
-        this.day = day;
-
-        this.hour = 0;
-        this.minute = 0;
-
-        this.calendar = Calendar.getInstance();
-        this.calendar.set(this.year, this.month, this.day,
-                this.hour, this.minute);
-    }
-
-    Datetime(int hour, int minute) {
-        this.hasDate = false;
-        this.year = 0;
-        this.month = 0;
-        this.day = 0;
-
-        this.hasTime = true;
-        this.hour = hour;
-        this.minute = minute;
-
-        this.calendar = Calendar.getInstance();
-        this.calendar.set(this.year, this.month, this.day,
-                this.hour, this.minute);
-    }
-
-    Datetime(int year, int month, int day, int hour, int minute) {
-        this.hasDate = true;
-        this.year = year;
-        this.month = month;
-        this.day = day;
-
-        this.hasTime = true;
-        this.hour = hour;
-        this.minute = minute;
-
-        this.calendar = Calendar.getInstance();
-        this.calendar.set(this.year, this.month, this.day,
-                this.hour, this.minute);
-    }
-
-    Datetime(String datetime_string) {
-        if (datetime_string.isEmpty()) {
+    /**
+     * String constructor. Creates the object by parsing the string passed.
+     * Uses default empty constructor {@link #Datetime()} if string is empty.
+     *
+     * @param datetime The string of the datetime. Can be obtained by the
+     *                 {@link #toString} method.
+     */
+    Datetime(String datetime) {
+        if (datetime.isEmpty()) {
+            // Set to default constructor
             this.hasDate = false;
-            this.year = 0;
-            this.month = 0;
-            this.day = 0;
-
             this.hasTime = false;
-            this.hour = 0;
-            this.minute = 0;
 
             this.calendar = Calendar.getInstance();
-            this.calendar.set(this.year, this.month, this.day,
-                    this.hour, this.minute);
-            return;
-        }
-        String[] datetime = datetime_string.split(" ");
-        String[] date = datetime[0].split("/");
-        String[] time = datetime[1].split(":");
-
-        this.year = Integer.parseInt(date[0]);
-        this.month = Integer.parseInt(date[1]);
-        this.day = Integer.parseInt(date[2]);
-
-        this.hour = Integer.parseInt(time[0]);
-        this.minute = Integer.parseInt(time[1]);
-
-        this.calendar = Calendar.getInstance();
-        this.calendar.set(this.year, this.month, this.day,
-                this.hour, this.minute);
-    }
-
-    boolean hasTime() { return hasTime; }
-
-    boolean hasDate() { return hasDate; }
-
-    Datetime add(Duration duration) {
-        this.calendar.toString();
-        this.calendar.add(Calendar.YEAR, duration.getYears());
-        this.calendar.add(Calendar.MONTH, duration.getMonths());
-        this.calendar.add(Calendar.DAY_OF_MONTH, duration.getDays());
-
-        this.calendar.add(Calendar.HOUR_OF_DAY, duration.getHours());
-        this.calendar.add(Calendar.MINUTE, duration.getMinutes());
-
-        this.year = calendar.get(Calendar.YEAR);
-        this.month = calendar.get(Calendar.MONTH);
-        this.day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        this.hour = calendar.get(Calendar.HOUR_OF_DAY);
-        this.minute = calendar.get(Calendar.MINUTE);
-
-        return new Datetime(this.year, this.month, this.day,
-                this.hour, this.minute);
-    }
-
-    Datetime subtract(Duration duration) {
-        this.calendar.add(Calendar.YEAR, -duration.getYears());
-        this.calendar.add(Calendar.MONTH, -duration.getMonths());
-        this.calendar.add(Calendar.DAY_OF_MONTH, -duration.getDays());
-
-        this.calendar.add(Calendar.HOUR_OF_DAY, -duration.getHours());
-        this.calendar.add(Calendar.MINUTE, -duration.getMinutes());
-
-        this.year = calendar.get(Calendar.YEAR);
-        this.month = calendar.get(Calendar.MONTH);
-        this.day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        this.hour = calendar.get(Calendar.HOUR_OF_DAY);
-        this.minute = calendar.get(Calendar.MINUTE);
-
-        return new Datetime(this.year, this.month, this.day,
-                this.hour, this.minute);
-    }
-
-    @Override
-    public String toString() {
-        return this.year + "/" + this.month + "/" + this.day + " " + this.hour + ":" + this.minute;
-    }
-
-    private String value_format(String value, int length) {
-        if (value.length() < length) {
-            String padding = "";
-            for (int i=0; i<length-value.length(); i++) {
-                padding += "0";
-            }
-            return padding + value;
+            this.calendar.set(0, 0, 0, 0, 0);
         } else {
-            return value;
+            // Parse data from string
+            String[] datetime_list = datetime.split(" ");
+            String[] date_list = datetime_list[0].split("/");
+            String[] time_list = datetime_list[1].split(":");
+
+            // Set values
+            this.calendar = Calendar.getInstance();
+            // Set date values
+            this.calendar.set(Calendar.YEAR, Integer.parseInt(date_list[0]));
+            this.calendar.set(Calendar.MONTH, Integer.parseInt(date_list[1]));
+            this.calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(date_list[2]));
+
+            // Set time values
+            this.calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time_list[0]));
+            this.calendar.set(Calendar.MINUTE, Integer.parseInt(time_list[1]));
+
+            // Set hasDate
+            this.hasDate = this.getDay() == 0;
+            // Set hasTime
+            this.hasTime = this.getHour() == 0;
         }
     }
 
-    public String toFormattedString() {
-        String formattedString = "";
-        if (this.hour != -1 && this.minute != -1) {
-            formattedString += value_format(Integer.toString(this.hour), 2);
-            formattedString += ":";
-            formattedString += value_format(Integer.toString(this.minute), 2);
-        }
-
-        if (this.year != -1 && this.month != -1 && this.day != -1) {
-            if (!formattedString.isEmpty()) {
-                formattedString += " ";
-            }
-            formattedString += value_format(Integer.toString(this.day), 2);
-            formattedString += "/";
-            formattedString += value_format(Integer.toString(this.month), 2);
-            formattedString += "/";
-            formattedString += this.year;
-        }
-        return formattedString;
+    // Getters and Setters
+    /**
+     * Gets the datetime's year.
+     * @return datetime's year
+     */
+    int getYear() {
+        return this.calendar.get(Calendar.YEAR);
+    }
+    /**
+     * Gets the datetime's month.
+     * @return datetime's month
+     */
+    int getMonth() {
+        return this.calendar.get(Calendar.MONTH);
+    }
+    /**
+     * Gets the datetime's day of the month.
+     * @return datetime's day of month
+     */
+    int getDay() {
+        return this.calendar.get(Calendar.DAY_OF_MONTH);
     }
 
-    // Getters and setters
+    /**
+     * Gets the datetime's 24 hour value.
+     * @return datetime's 24 hour value
+     */
+    int getHour() {
+        return this.calendar.get(Calendar.HOUR_OF_DAY);
+    }
+    /**
+     * Gets the datetime's minute
+     * @return datetime's minute
+     */
+    int getMinute() {
+        return this.calendar.get(Calendar.MINUTE);
+    }
+
+    /**
+     * Gets the value of {@link #hasDate}, which is set to true once date is
+     * set by {@link #setYear(int)}, {@link #setMonth(int)},
+     * {@link #setDay(int)} or during construction by
+     * {@link #Datetime(Datetime)} or {@link #Datetime(String)}.
+     * @return value of {@link #hasDate}
+     */
+    boolean getHasDate() {
+        return this.hasDate;
+    }
+    /**
+     * Gets the value of {@link #hasTime}, which is set to true once date is
+     * set by {@link #setHour(int)}, {@link #setMinute(int)}, or during
+     * construction by {@link #Datetime(Datetime)} or
+     * {@link #Datetime(String)}.
+     * @return value of {@link #hasDate}
+     */
+    boolean getHasTime() {
+        return this.hasTime;
+    }
+
+    /**
+     * Gets the datetime's calendar time in milliseconds.
+     * @return datetime calendar time in milliseconds
+     */
+    long getMillis() {
+        return this.calendar.getTimeInMillis();
+    }
+
+    /**
+     * {@link #getYear()}
+     * @param year datetime's year
+     */
     void setYear(int year) {
         hasDate = true;
-        this.year = year;
+        this.calendar.set(Calendar.YEAR, year);
     }
+    /**
+     * {@link #getMonth()}
+     * @param month datetime's month
+     */
     void setMonth(int month) {
         hasDate = true;
-        this.month = month;
+        this.calendar.set(Calendar.MONTH, month);
     }
+    /**
+     * {@link #getDay()}
+     * @param day datetime's day
+     */
     void setDay(int day) {
         hasDate = true;
-        this.day = day;
-    }
-    void setHour(int hour) {
-        this.hasTime = true;
-        this.hour = hour;
-    }
-    void setMinute(int minute) {
-        this.hasTime = true;
-        this.minute = minute;
+        this.calendar.set(Calendar.DAY_OF_MONTH, day);
     }
 
-    int getYear() {
-        return this.year;
+    /**
+     * {@link #getHour()}
+     * @param hour datetime's hour
+     */
+    void setHour(int hour) {
+        this.hasTime = true;
+        this.calendar.set(Calendar.HOUR_OF_DAY, hour);
     }
-    int getMonth() {
-        return this.month;
+    /**
+     * {@link #getMinute()}
+     * @param minute datetime's minute
+     */
+    void setMinute(int minute) {
+        this.hasTime = true;
+        this.calendar.set(Calendar.MINUTE, minute);
     }
-    int getDay() {
-        return this.day;
+
+    /**
+     * {@link #getMillis()}
+     * @param millis datetime's time in milliseconds
+     */
+    void setMillis(long millis) {
+        this.calendar.setTimeInMillis(millis);
     }
-    int getHour() {
-        return this.hour;
+
+    /**
+     * Returns the a new datetime instance with the duration added.
+     *
+     * @see Duration
+     * @param duration the duration to add
+     * @return new datetime instance with duration added
+     */
+    Datetime add(Duration duration) {
+        //TODO: Check dependency with Duration
+        // Clone current calendar instance to calculate new datetime
+        Calendar calendar = (Calendar) this.calendar.clone();
+        // Add the duration to the calendar
+        calendar.add(Calendar.YEAR, duration.getYears());
+        calendar.add(Calendar.MONTH, duration.getMonths());
+        calendar.add(Calendar.DAY_OF_MONTH, duration.getDays());
+
+        calendar.add(Calendar.HOUR_OF_DAY, duration.getHours());
+        calendar.add(Calendar.MINUTE, duration.getMinutes());
+        // New datetime instance
+        Datetime datetime = new Datetime();
+        // Set calendar values
+        datetime.setYear(calendar.get(Calendar.YEAR));
+        datetime.setMonth(calendar.get(Calendar.MONTH));
+        datetime.setDay(calendar.get(Calendar.DAY_OF_MONTH));
+
+        datetime.setHour(calendar.get(Calendar.HOUR_OF_DAY));
+        datetime.setMinute(calendar.get(Calendar.MINUTE));
+
+        return datetime;
     }
-    int getMinute() {
-        return this.minute;
+
+    /**
+     * Returns the a new datetime instance with the duration subtracted.
+     *
+     * @see Duration
+     * @param duration the duration to subtract
+     * @return new datetime instance with duration subtracted
+     */
+    Datetime subtract(Duration duration) {
+        //TODO: Check dependency with Duration
+        // Clone current calendar instance to calculate new datetime
+        Calendar calendar = (Calendar) this.calendar.clone();
+        // Subtract the duration to the calendar
+        calendar.add(Calendar.YEAR, -duration.getYears());
+        calendar.add(Calendar.MONTH, -duration.getMonths());
+        calendar.add(Calendar.DAY_OF_MONTH, -duration.getDays());
+
+        calendar.add(Calendar.HOUR_OF_DAY, -duration.getHours());
+        calendar.add(Calendar.MINUTE, -duration.getMinutes());
+        // New datetime instance
+        Datetime datetime = new Datetime();
+        // Set calendar values
+        datetime.setYear(calendar.get(Calendar.YEAR));
+        datetime.setMonth(calendar.get(Calendar.MONTH));
+        datetime.setDay(calendar.get(Calendar.DAY_OF_MONTH));
+
+        datetime.setHour(calendar.get(Calendar.HOUR_OF_DAY));
+        datetime.setMinute(calendar.get(Calendar.MINUTE));
+
+        return datetime;
+    }
+
+    /**
+     * Returns the datetime's calendar year, month, day, hour and minute
+     * values in the format "Y/M/D H:M" where the values do not have a fixed
+     * width (they are not padded with zeroes).
+     *
+     * @return datetime's string equivalent
+     */
+    @Override
+    public String toString() {
+        return this.getYear() + "/" + this.getMonth() + "/" + this.getDay() + " " +
+                this.getHour() + ":" + this.getMinute();
+    }
+
+    /**
+     * Returns the datetime's calendar year, month, day, hour and minute
+     * depending on whether they exist. The values are formatted in the format
+     * "DD/MM/YY HH:MM" when all values are present, "DD/MM/YY" when only the
+     * date is present and "HH:MM" when only the time is present.
+     *
+     * @return datetime formatted string
+     */
+    String toFormattedString() {
+        String formattedString = "";
+        // Add date if it is present
+        if (this.getHasDate()) {
+            formattedString += String.format(Locale.getDefault(),
+                    "%1$ty/%1$tm/%1$td", this.calendar);
+        }
+        // Add time if it is present
+        if (this.getHasTime()) {
+            formattedString += String.format(Locale.getDefault(),
+                    "%1$tH:%1$tM", this.calendar);
+        }
+
+        return formattedString;
     }
 }
