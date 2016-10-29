fd8bd81fa31c4cb370e9d9d994f8a78cd52d2d3c
Zhu Zhan Yan <program.nom@gmail.com>
2016-10-29 16:55:00 +0800

    Added PVRSetting

---
+++ IOS/Prevoir/PVRSetting.swift
@@ -0,0 +1,15 @@
+//
+//  PVRSetting.swift
+//  Prevoir
+//
+//  Created by Zhu Zhan Yan on 29/10/16.
+//  Copyright © 2016 SSTInc. All rights reserved.
+//
+
+import UIKit
+
+public enum PVRSetting:String
+{
+    //UI Settings
+    case ui_shake =  "pvr_set_ui_shake" /* Whether shake to refresh is enabled*/
+}
